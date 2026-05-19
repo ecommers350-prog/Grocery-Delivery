31fe1532fc3cd0bb156f3f4372d20652408e05ce
Grocery User <user@example.com>
2026-05-19 21:13:20 +0530

    backend completed

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,9 @@ import uploadRouter from "./routes/uploadRoutes.js";
 import orderRouter from "./routes/orderRouters.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import addressRouter from "./routes/addressRoutes.js";
+import adminRouter from "./routes/adminRouter.js";
+import deliveryPartnerRouter from "./routes/deliveryPatenerRoutes.js";
 
 const app = express();
 
@@ -25,6 +28,9 @@ app.use('/api/products', productRouter)
 app.use('/api/upload', uploadRouter)
 app.use('/api/orders', orderRouter)
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/addresses', addressRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/delivery', deliveryPartnerRouter)
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
